9a11a728d4fae11277353bb23e200a326fcc8070
amirkhosrwiy <amirkhosravi14001385@gmail.com>
2023-03-16 04:42:41 -0700

    score  players in  16:10 1401/12/25

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,22 +12,42 @@ class _HomeState extends State<Home> {
 
   List<String> xOrOList = ["", "", "", "", "", "", "", "", ""];
 
+  int filledBoxes = 0;
+
+  bool GameHasResult = false;
+
+  int scoreX = 0;
+  int scoreO = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(
-          child: Text(
-            'Dooz',
-            style: TextStyle(
-              color: Color(0xff1A1D27),
-              // fontFamily:
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
+        actions: [
+          IconButton(
+            onPressed: () {
+              clearGame();
+            },
+            icon: Icon(Icons.refresh),
+            color: Color(0xffE23E58),
+            iconSize: 30,
+          ),
+        ],
+        title: Padding(
+          padding: const EdgeInsets.only(left: 50),
+          child: Center(
+            child: Text(
+              'Dooz',
+              style: TextStyle(
+                color: Color(0xff30CCFF),
+                // fontFamily:
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
         ),
-        backgroundColor: Color(0xff30CCFF),
+        backgroundColor: Color(0xff1A1D27),
         elevation: 20,
       ),
       backgroundColor: Color(0xff1A1D27),
@@ -108,8 +128,10 @@ class _HomeState extends State<Home> {
 
       if (isTurnO) {
         xOrOList[index] = 'O';
+        filledBoxes = filledBoxes + 1;
       } else {
         xOrOList[index] = 'X';
+        filledBoxes = filledBoxes + 1;
       }
       isTurnO = !isTurnO;
 
@@ -121,49 +143,61 @@ class _HomeState extends State<Home> {
     if (xOrOList[0] == xOrOList[1] &&
         xOrOList[0] == xOrOList[2] &&
         xOrOList[0] != '') {
-      print('Wineer is ' + xOrOList[0]);
+      setResult(xOrOList[0], 'Winner is ' + xOrOList[0]);
+      return;
     }
 
     if (xOrOList[3] == xOrOList[4] &&
         xOrOList[3] == xOrOList[5] &&
         xOrOList[3] != '') {
-      print('Wineer is ' + xOrOList[3]);
+      setResult(xOrOList[3], 'Winner is ' + xOrOList[3]);
+      return;
     }
 
     if (xOrOList[6] == xOrOList[7] &&
         xOrOList[6] == xOrOList[8] &&
         xOrOList[6] != '') {
-      print('Wineer is ' + xOrOList[6]);
+      setResult(xOrOList[6], 'Winner is ' + xOrOList[6]);
+      return;
     }
 
     if (xOrOList[0] == xOrOList[3] &&
         xOrOList[0] == xOrOList[6] &&
         xOrOList[0] != '') {
-      print('Wineer is ' + xOrOList[0]);
+      setResult(xOrOList[0], 'Winner is ' + xOrOList[0]);
+      return;
     }
 
     if (xOrOList[1] == xOrOList[4] &&
         xOrOList[1] == xOrOList[7] &&
         xOrOList[1] != '') {
-      print('Wineer is ' + xOrOList[1]);
+      setResult(xOrOList[1], 'Winner is ' + xOrOList[1]);
+      return;
     }
 
     if (xOrOList[2] == xOrOList[5] &&
         xOrOList[2] == xOrOList[8] &&
         xOrOList[2] != '') {
-      print('Wineer is ' + xOrOList[2]);
+      setResult(xOrOList[2], 'Winner is ' + xOrOList[2]);
+      return;
     }
 
     if (xOrOList[2] == xOrOList[4] &&
         xOrOList[2] == xOrOList[6] &&
         xOrOList[2] != '') {
-      print('Wineer is ' + xOrOList[2]);
+      setResult(xOrOList[2], 'Winner is ' + xOrOList[2]);
+      return;
     }
 
     if (xOrOList[0] == xOrOList[4] &&
         xOrOList[0] == xOrOList[8] &&
         xOrOList[0] != '') {
-      print('Wineer is ' + xOrOList[0]);
+      setResult(xOrOList[0], 'Winner is ' + xOrOList[0]);
+      return;
+    }
+
+    if (filledBoxes == 9) {
+      setResult('', 'Draw');
     }
   }
 
@@ -187,7 +221,7 @@ class _HomeState extends State<Home> {
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Text(
-                  '0',
+                  '$scoreO',
                   style: TextStyle(
                     color: Color(0xffE23E58),
                     fontSize: 25,
@@ -215,7 +249,7 @@ class _HomeState extends State<Home> {
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Text(
-                  '2',
+                  '$scoreX',
                   style: TextStyle(
                     color: Color(0xff30CCFF),
                     fontSize: 25,
@@ -228,5 +262,32 @@ class _HomeState extends State<Home> {
         ),
       ],
     );
+  }
+
+  void setResult(String winner, String title) {
+    setState(
+      () {
+        GameHasResult = true;
+
+        if (winner == 'X') {
+          scoreX = scoreX + 1;
+        } else if (winner == 'O') {
+          scoreO = scoreO + 1;
+        } else {
+          scoreX = scoreX + 0;
+          scoreO = scoreO + 0;
+        }
+      },
+    );
+  }
+
+  void clearGame() {
+    setState(() {
+      for (int i = 0; i < xOrOList.length; i++) {
+        xOrOList[i] = '';
+      }
+    });
+
+    filledBoxes = 0;
   }
 }
