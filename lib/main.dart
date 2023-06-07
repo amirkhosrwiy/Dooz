b9620ce68fb0839254c0a27556d09978561c14a4
amirkhosrwiy <amirkhosravi14001385@gmail.com>
2023-06-07 04:20:05 -0700

    completed Splash Screen in 14:50 1402/3/17

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:application_dooz/screens/selection_game.dart';
 import 'package:flutter/material.dart';
 // import 'package:lottie/lottie.dart';
 import 'package:flutter/services.dart';
+import 'package:lottie/lottie.dart';
 
 void main() async {
   runApp(const Application());
@@ -38,7 +39,7 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
 
     Timer(
-      Duration(seconds: 7),
+      Duration(seconds: 11),
       () => Navigator.of(context).pushReplacement(
         MaterialPageRoute(
           builder: (BuildContext context) => GotoHome_Screen(),
@@ -50,80 +51,92 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              // crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Center(
-                  child: Image.asset(
-                    'images/logo2.png',
-                    width: 150,
-                    height: 150,
-                  ),
+            child: Padding(
+              padding: const EdgeInsets.only(top: 240),
+              child: Image.asset(
+                'images/logo.png',
+                width: 150,
+                height: 150,
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'بازی دوز',
+            style: TextStyle(
+              color: Color(0xff30CCFF),
+              fontSize: 50,
+              fontWeight: FontWeight.bold,
+              // fontWeight: FontWeight.bold,
+              fontFamily: 'IRAN',
+              shadows: [
+                Shadow(
+                  blurRadius: 20.0, // shadow blur
+                  color: Color(0xff30CCFF), // shadow color
+                  offset: Offset(3.0, 2.0), // how much shadow will be shown
                 ),
-                SizedBox(height: 20),
-                Text(
-                  'بازی دوز',
-                  style: TextStyle(
-                    color: Color(0xff30CCFF),
-                    fontSize: 40,
-                    // fontWeight: FontWeight.bold,
-                    fontFamily: 'IRAN',
-                    shadows: [
-                      Shadow(
-                        blurRadius: 20.0, // shadow blur
-                        color: Color(0xff30CCFF), // shadow color
-                        offset:
-                            Offset(3.0, 2.0), // how much shadow will be shown
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 20),
-                Center(
-                  child: Text(
-                    'اپلیکیشنی برای سرگرمی',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: 'IRAN',
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                      shadows: [
-                        Shadow(
-                          blurRadius: 20.0, // shadow blur
-                          color: Colors.white, // shadow color
-                          offset:
-                              Offset(3.0, 2.0), // how much shadow will be shown
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                SizedBox(height: 260),
-                // Center(
-                //   child: Text(
-                //     '',
-                //     style: TextStyle(
-                //       color: Color(0xff00D7FF),
-                //       fontFamily: 'QT',
-                //       fontSize: 20,
-                //       fontWeight: FontWeight.bold,
-                //       shadows: [
-                //         Shadow(
-                //           blurRadius: 20.0, // shadow blur
-                //           color: Color(0xff30CCFF), // shadow color
-                //           offset:
-                //               Offset(3.0, 2.0), // how much shadow will be shown
-                //         ),
-                //       ],
-                //     ),
-                //   ),
-                // ),
               ],
             ),
+          ),
+          SizedBox(height: 10),
+          Center(
+            child: Text(
+              'اپلیکیشنی برای سرگرمی',
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'IRAN',
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                shadows: [
+                  Shadow(
+                    blurRadius: 20.0, // shadow blur
+                    color: Color(0xffE23E58), // shadow color
+                    offset: Offset(3.0, 2.0), // how much shadow will be shown
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Column(
+            children: [
+              Stack(
+                children: [
+                  Positioned(
+                    top: 140,
+                    child: Lottie.asset('assets/lottie/15.json',
+                        width: 200, height: 200),
+                  ),
+                  Lottie.asset('assets/lottie/18.json',
+                      width: 200, height: 300),
+                  Positioned(
+                    top: 260,
+                    left: 55,
+                    child: Text(
+                      'شکیبا باشید',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontFamily: 'IRAN',
+                        fontWeight: FontWeight.bold,
+                        shadows: [
+                          Shadow(
+                            blurRadius: 20.0, // shadow blur
+                            color: Color(0xffE23E58), // shadow color
+                            offset: Offset(
+                                3.0, 2.0), // how much shadow will be shown
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
           ),
         ],
       ),
