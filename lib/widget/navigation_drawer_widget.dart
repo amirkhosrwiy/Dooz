78efa27a83bf67567a06ed8cb6031492d5fd4528
amirkhosrwiy <amirkhosravi14001385@gmail.com>
2023-06-20 00:36:43 -0700

    new vrsion SnackBar in 11:10  1402/3/30

--- lib/widget/navigation_drawer_widget.dart
+++ lib/widget/navigation_drawer_widget.dart
@@ -1,6 +1,7 @@
 import 'package:application_dooz/pages/help_game_screen.dart';
 import 'package:application_dooz/screens/about_screen.dart';
 import 'package:application_dooz/screens/flash.dart';
+import 'package:application_dooz/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -108,95 +109,12 @@ class NavigationDrawerWidget extends StatelessWidget {
         );
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
-            content: Stack(
-              clipBehavior: Clip.none,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(16),
-                  height: 90,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      begin: Alignment.centerRight,
-                      end: Alignment.centerLeft,
-                      colors: [
-                        Color.fromARGB(255, 77, 157, 238),
-                        Color.fromARGB(255, 137, 35, 52),
-                      ],
-                      stops: [0.1, 0.7],
-                    ),
-                    color: Color(0xffE23E58),
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: Row(
-                    children: [
-                      const SizedBox(height: 48),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Text(
-                              'Oh Snap!',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 18,
-                              ),
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                            Text(
-                              'Flutter default SnackBar is showing',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 12,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Positioned(
-                  bottom: -30,
-                  left: -10,
-                  child: ClipRRect(
-                    borderRadius: const BorderRadius.only(
-                      bottomLeft: Radius.circular(20),
-                    ),
-                    child: SvgPicture.asset(
-                      "assets/icons/1.svg",
-                      height: 100,
-                      width: 100,
-                      color: Colors.white.withOpacity(0.5),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  top: -20,
-                  right: 10,
-                  child: Stack(
-                    children: [
-                      SvgPicture.asset(
-                        "assets/icons/1.svg",
-                        height: 80,
-                        width: 80,
-                        color: Colors.white.withOpacity(0.5),
-                      ),
-                    ],
-                  ),
-                ),
-                Positioned(
-                  top: -20,
-                  left: 0,
-                  child: SvgPicture.asset(
-                    "assets/icons/close1.svg",
-                    height: 50,
-                  ),
-                ),
-              ],
+            content: CustomSnackBarContent(
+              errorText: "این ویژگی اکنون در دسترس نیست",
             ),
             behavior: SnackBarBehavior.floating,
             backgroundColor: Colors.transparent,
+            duration: Duration(seconds: 10),
           ),
         );
         break;
@@ -225,5 +143,122 @@ class NavigationDrawerWidget extends StatelessWidget {
       //       .push(MaterialPageRoute(builder: (context) => SettingsScreen()));
       //   break;
     }
+  }
+}
+
+class CustomSnackBarContent extends StatelessWidget {
+  const CustomSnackBarContent({
+    Key? key,
+    required this.errorText,
+  }) : super(key: key);
+
+  final String errorText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Container(
+            padding: EdgeInsets.all(16),
+            height: 90,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.centerRight,
+                end: Alignment.centerLeft,
+                colors: [
+                  Color.fromARGB(255, 77, 157, 238),
+                  Color.fromARGB(255, 137, 35, 52),
+                ],
+                stops: [0.1, 0.7],
+              ),
+              color: Color(0xffE23E58),
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: Row(
+              children: [
+                SizedBox(width: 20),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        '!خطا دریافت شد',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontFamily: 'IRAN',
+                          fontWeight: FontWeight.bold,
+                        ),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      Spacer(),
+                      Text(
+                        errorText,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22,
+                          fontFamily: 'IRAN',
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: -30,
+            left: -10,
+            child: ClipRRect(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+              ),
+              child: SvgPicture.asset(
+                "assets/icons/1.svg",
+                height: 100,
+                width: 100,
+                color: Colors.white.withOpacity(0.5),
+              ),
+            ),
+          ),
+          Positioned(
+            top: -30,
+            right: 10,
+            child: Stack(
+              children: [
+                SvgPicture.asset(
+                  "assets/icons/1.svg",
+                  height: 80,
+                  width: 80,
+                  color: Colors.white.withOpacity(0.5),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: -20,
+            left: 0,
+            child: InkWell(
+              child: SvgPicture.asset(
+                "assets/icons/close1.svg",
+                height: 50,
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: ((BuildContext context) => Home()),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
